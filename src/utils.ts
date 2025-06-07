c2a256ba4375618517030ffe85b948e2a03767ff
Naatin <76672482+naatin777@users.noreply.github.com>
2025-06-07 16:40:02 +0900

    feat: add utility functions for path and LaTeX escaping

---
+++ src/utils.ts
@@ -0,0 +1,31 @@
+import * as path from 'path';
+
+export function toPosixPath(p: string): string {
+    return path.normalize(p).split(/[\\\/]/g).join(path.posix.sep);
+}
+
+export function escapeLatex(text: string): string {
+    return text
+        .replace(/\\/g, '\\textbackslash ')
+        .replace(/%/g, '\\%')
+        .replace(/{/g, '\\{')
+        .replace(/}/g, '\\}')
+        .replace(/&/g, '\\&')
+        .replace(/#/g, '\\#')
+        .replace(/\$/g, '\\$')
+        .replace(/\^/g, '\\textasciicircum ')
+        .replace(/~/g, '\\textasciitilde ')
+        .replace(/_/g, '\\_')
+        .replace(/\|/g, '\\textbar ')
+        .replace(/</g, '\\textless ')
+        .replace(/>/g, '\\textgreater ');
+}
+
+export function escapeLatexLabel(text: string): string {
+    return text
+        .replace(/\\/g, '')
+        .replace(/%/g, '')
+        .replace(/{/g, '')
+        .replace(/}/g, '')
+        .replace(/#/g, '');
+}
