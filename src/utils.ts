a1cfc725e390e58af081dca32efe36b117d2c2df
Naatin <76672482+naatin777@users.noreply.github.com>
2025-06-08 15:49:37 +0900

    feat: add utility function for array transposition

--- src/utils.ts
+++ src/utils.ts
@@ -29,3 +29,7 @@ export function escapeLatexLabel(text: string): string {
         .replace(/}/g, '')
         .replace(/#/g, '');
 }
+
+export function transpose<T>(a: T[][]): T[][] {
+    return a[0].map((_, c) => a.map(r => r[c]));
+}
