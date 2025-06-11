c9b6603beab68d02350a9964c7871fc82629bc78
Naatin <76672482+naatin777@users.noreply.github.com>
2025-06-11 23:22:43 +0900

    feat: add PNG to PDF conversion functionality

---
+++ src/image_to_pdf.ts
@@ -0,0 +1,43 @@
+import { getInkscapeCommand, getShell } from './configuration';
+import { execSync } from 'child_process';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as vscode from 'vscode';
+
+export function pngToPdf(
+    inputPath: string,
+    outputPath: string,
+    workspaceFolder: string = '',
+): void {
+    if (!fs.existsSync(inputPath)) {
+        vscode.window.showErrorMessage(`File does not exist: ${inputPath}`);
+        return;
+    }
+
+    const fileName = path.basename(inputPath, path.extname(inputPath));
+    const folderName = path.dirname(inputPath);
+    const replacedOutputPath = outputPath
+        .replace(/\${fileName}/g, fileName)
+        .replace(/\${folderName}/g, folderName)
+        .replace(/\${workspaceFolder}/g, workspaceFolder);
+    const replacedOutputFolderPath = path.dirname(replacedOutputPath);
+
+    if (!fs.existsSync(replacedOutputFolderPath)) {
+        fs.mkdirSync(replacedOutputFolderPath, { recursive: true });
+    }
+    try {
+        execSync(
+            `${getInkscapeCommand()} "${path.normalize(inputPath)}" -o "${path.normalize(replacedOutputPath)}" --export-type=pdf --export-area-drawing`,
+            {
+                'cwd': folderName,
+                'shell': getShell()
+            }
+        );
+    } catch (error) {
+        if (error instanceof Error) {
+            vscode.window.showErrorMessage(`Failed to convert PNG to PDF: ${inputPath} - ${error.message}`);
+        }
+
+        return;
+    }
+}
