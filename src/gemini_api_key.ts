a5d8d38b8904fcdbb3917f2f048bc228bec2d5bf
Naatin <76672482+naatin777@users.noreply.github.com>
2025-08-04 15:53:31 +0900

    feat: add Gemini API key configuration file

---
+++ src/gemini_api_key.ts
@@ -0,0 +1,18 @@
+import * as vscode from 'vscode';
+
+const GEMINI_API_KEY = 'latex-graphics-helper.gemini-api-key';
+
+export async function storeGeminiApiKey(
+    secretStorage: vscode.SecretStorage,
+    value: string
+) {
+    await secretStorage.store(GEMINI_API_KEY, value);
+}
+
+export async function getGeminiApiKey(secretStorage: vscode.SecretStorage) {
+    return await secretStorage.get(GEMINI_API_KEY);
+}
+
+export async function deleteGeminiApiKey(secretStorage: vscode.SecretStorage) {
+    await secretStorage.delete(GEMINI_API_KEY);
+}
