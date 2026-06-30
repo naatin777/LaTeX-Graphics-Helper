ccacdd4624ef89b1cbccbea1d3fea39b6f534dc4
naatin777 <76672482+naatin777@users.noreply.github.com>
2026-06-30 16:04:16 +0900

    Normalize pdfcrop paths on Windows

--- src/core/crop_pdf.ts
+++ src/core/crop_pdf.ts
@@ -6,6 +6,10 @@ import { createFolder } from '../utils/create_folder';
 import { execFileInWorkspace } from '../utils/exec_file_in_workspace';
 import { generatePathFromTemplate } from '../utils/generate_path_from_template';
 
+function toPdfcropPath(filePath: PdfPath): PdfPath {
+    return (process.platform === 'win32' ? filePath.replaceAll('\\', '/') : filePath) as PdfPath;
+}
+
 export async function cropPdf(
     appConfig: AppConfig,
     inputPath: PdfPath,
@@ -14,6 +18,10 @@ export async function cropPdf(
 ): Promise<PdfPath> {
     const outputPath = generatePathFromTemplate(outputTemplatePath, inputPath, workspaceFolder);
     await createFolder(outputPath);
-    await execFileInWorkspace(appConfig.execPathPdfcrop, [inputPath, outputPath], workspaceFolder);
+    await execFileInWorkspace(
+        appConfig.execPathPdfcrop,
+        [toPdfcropPath(inputPath), toPdfcropPath(outputPath as PdfPath)],
+        workspaceFolder,
+    );
     return outputPath as PdfPath;
 }
