7a99478a1914cf2bbecff04b2e64eedff1f47405
naatin777 <76672482+naatin777@users.noreply.github.com>
2026-07-21 22:44:16 +0900

    feat: add LaTeX insertion template settings and renderer
    - Add insertLatex.pdfTemplate / imageTemplate settings
    - Create template renderer with path/name/ext/page/dir variables
    - Default templates for PDF (includegraphics) and images (resizebox)
    - NLS messages (en/ja)

---
+++ src/edit_provider/latex_template.ts
@@ -0,0 +1,48 @@
+import * as vscode from 'vscode';
+
+export interface TemplateContext {
+  path: string;
+  name: string;
+  ext: string;
+  page?: number;
+  dir: string;
+}
+
+const DEFAULT_PDF_TEMPLATE = [
+  '\\begin{figure}[H]',
+  '  \\centering',
+  '  \\includegraphics[width=\\linewidth]{${path}}',
+  '  \\caption{${name}}',
+  '  \\label{fig:${name}}',
+  '\\end{figure}',
+].join('\n');
+
+const DEFAULT_IMAGE_TEMPLATE = [
+  '\\begin{figure}[H]',
+  '  \\centering',
+  '  \\resizebox{\\linewidth}{!}{\\includegraphics{${path}}}',
+  '  \\caption{${name}}',
+  '  \\label{fig:${name}}',
+  '\\end{figure}',
+].join('\n');
+
+export function renderTemplate(template: string, context: TemplateContext): string {
+  return template
+    .replaceAll('${path}', context.path)
+    .replaceAll('${name}', context.name)
+    .replaceAll('${ext}', context.ext)
+    .replaceAll('${page}', context.page !== undefined ? String(context.page) : '1')
+    .replaceAll('${dir}', context.dir);
+}
+
+export function getPdfTemplate(configuration: vscode.WorkspaceConfiguration): string {
+  return (
+    configuration.get<string>('insertLatex.pdfTemplate') || DEFAULT_PDF_TEMPLATE
+  );
+}
+
+export function getImageTemplate(configuration: vscode.WorkspaceConfiguration): string {
+  return (
+    configuration.get<string>('insertLatex.imageTemplate') || DEFAULT_IMAGE_TEMPLATE
+  );
+}
