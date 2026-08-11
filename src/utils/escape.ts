330ca98318dd4a0a1c1218940304cb933e72dc74
Naatin <76672482+naatin777@users.noreply.github.com>
2026-08-11 12:02:45 +0900

    fix: address CodeQL security alerts (#394)
    Consolidate LaTeX escaping and restrict test workflow token permissions.

--- src/utils/escape.ts
+++ src/utils/escape.ts
@@ -1,25 +1,25 @@
+const LATEX_ESCAPES: Readonly<Record<string, string>> = {
+    '\\': '\\textbackslash ',
+    '%': '\\%',
+    '{': '\\{',
+    '}': '\\}',
+    '&': '\\&',
+    '#': '\\#',
+    $: '\\$',
+    '^': '\\textasciicircum ',
+    '~': '\\textasciitilde ',
+    _: '\\_',
+    '|': '\\textbar ',
+    '<': '\\textless ',
+    '>': '\\textgreater ',
+};
+
+const LATEX_SPECIAL_CHARACTERS = /[\\%{}&#$^~_|<>]/g;
+
 export function escapeLatex(text: string): string {
-    return text
-        .replace(/\\/g, '\\textbackslash ')
-        .replace(/%/g, '\\%')
-        .replace(/{/g, '\\{')
-        .replace(/}/g, '\\}')
-        .replace(/&/g, '\\&')
-        .replace(/#/g, '\\#')
-        .replace(/\$/g, '\\$')
-        .replace(/\^/g, '\\textasciicircum ')
-        .replace(/~/g, '\\textasciitilde ')
-        .replace(/_/g, '\\_')
-        .replace(/\|/g, '\\textbar ')
-        .replace(/</g, '\\textless ')
-        .replace(/>/g, '\\textgreater ');
+    return text.replace(LATEX_SPECIAL_CHARACTERS, (character) => LATEX_ESCAPES[character]);
 }
 
 export function escapeLatexLabel(text: string): string {
-    return text
-        .replace(/\\/g, '')
-        .replace(/%/g, '')
-        .replace(/{/g, '')
-        .replace(/}/g, '')
-        .replace(/#/g, '');
+    return text.replace(/[\\%{}#]/g, '');
 }
